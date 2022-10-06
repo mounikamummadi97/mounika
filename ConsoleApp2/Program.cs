cc5b9e1362b3e169a03e1a2ad872c0a1c00c7acd
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-06 17:04:41 +0530

    fvhnghfg

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Console.WriteLine("data types");
+namespace MyAppplication
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            long myNum = 150000000L;
+            Console.WriteLine(myNum);
+            float myNum1 = 6.87F;
+            Console.WriteLine(myNum1);
+            double myNum2 = 64.33D;
+            char  val= 'm';
+            Console.WriteLine(myNum2);
+            int myInt = 5;
+            double myDouble1 = myInt;
+            Console.WriteLine(myDouble1);
+            double myDouble2 = 9.45;
+            int myInt1 = (int)myDouble2;
+            Console.WriteLine(myInt1);
+            Console.WriteLine(myDouble2);
+
+        }
+    }
+}
